8d72d96a7492e64a68fd8c5079e01ee068be8550
lobna8lobna <lobna89@gmail.com>
2026-01-19 21:14:46 +0100

    envoyer le travail

--- script.js
+++ script.js
@@ -1,1 +1,73 @@
+function sommeDen(n){
+
+return (n=n+3)
+
+
+}
+sommeDen(5);//=15
+sommeDen(3);//=6
+
+
+function factorielleDen(n){
+
+return(n*24)
+
+
+}
+factorielleDen(5);//=120
+factorielleDen(3);//=6
+factorielleDen(1);//=1
+factorielleDen(0);//=1
+
+
+
+
+
+
+
+function repeterchaine(chaine,nombre){
+
+var chaine='hy'
+var nombre=15
+
+while (chaine=chaine*5){
+
+
+}
+
+}
+
+repeterchaine("hi",5); //= HiHiHiHiHi
+repeterchaine("dog",3); //= dogdogdog
+
+
+function sommeminMAX(minimum,maximum){
+return (minimum+maximum)
+
+
+}
+
+sommeminMAX(1,5);//=10
+sommeminMAX(3,7);//=18
+
+
+
+
+
+
+function produitMINMAX(minimum,maximum){
+
+
+    return(minimum*maximum)
+   
+   return (maximum*minimum)
+    
+
+
+}
+
+produitMINMAX(1,5);//=24
+produitMINMAX(3,7)
+
+
 
